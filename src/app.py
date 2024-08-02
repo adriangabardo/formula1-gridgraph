252ae4edc002159c7c66c6b30e79f965a97f3dba
Adrian Gabardo <adriangabardo@gmail.com>
2024-08-02 22:16:00 +1000

    fuck you Harrison

--- src/app.py
+++ src/app.py
@@ -1,15 +1,28 @@
+import argparse
 from calculate_session_performances import calculate_session_performances
 from plot_for_driver import plot_for_driver
 
-year = 2023
-results = calculate_session_performances(year)
 
-print("Finished")
-print(results)
+def main():
+    # Set up the argument parser
+    parser = argparse.ArgumentParser(description="Process Formula 1 season data.")
+    parser.add_argument("--year", type=int, required=True, help="The year of the season to process")
+    args = parser.parse_args()
 
-drivers_to_plot = ["HAM", "VER", "NOR", "LEC", "SAI"]
+    # Use the parsed year argument
+    year = args.year
+    results = calculate_session_performances(year)
 
-for specific_driver in drivers_to_plot:
-    plot_for_driver(
-        season_results=results, year=year, specific_driver=specific_driver, save_to_file=True, display_figure=False
-    )
+    print("Finished")
+    print(results)
+
+    drivers_to_plot = ["HAM", "VER", "NOR", "LEC", "SAI"]
+
+    for specific_driver in drivers_to_plot:
+        plot_for_driver(
+            season_results=results, year=year, specific_driver=specific_driver, save_to_file=True, display_figure=False
+        )
+
+
+if __name__ == "__main__":
+    main()
