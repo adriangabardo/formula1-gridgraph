1b2ca6e6e34e03af6dc31c6cc2df98371aa922d9
Adrian Gabardo <adriangabardo@gmail.com>
2024-08-04 19:26:50 +1000

    more comprehensive graph

--- src/plot_for_driver.py
+++ src/plot_for_driver.py
@@ -1,6 +1,14 @@
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 from pandas import DataFrame
 import os
+
+custom_lines = [
+    Line2D([0], [0], color="green", lw=2),
+    Line2D([0], [0], color="red", lw=2),
+    Line2D([0], [0], marker="D", color="w", markerfacecolor="C0", markersize=10),
+    Line2D([0], [0], marker="o", color="w", markerfacecolor="C1", markersize=10),
+]
 
 
 def plot_for_driver(
@@ -16,22 +24,32 @@ def plot_for_driver(
     races = driver_results_df["EventName"]
     grid_positions = driver_results_df["GridPosition"]
     lap_positions = driver_results_df["Position"]
+    differences = driver_results_df["Difference"]
 
     # Create the stem plot
     plt.figure(figsize=(12, 6))
 
-    plt.stem(races, grid_positions, linefmt="C0-", markerfmt="D", basefmt=" ", bottom=1, orientation="horizontal")
-    plt.stem(races, lap_positions, linefmt="C1-", markerfmt="C1o", basefmt=" ", bottom=1, orientation="horizontal")
+    for race, grid_pos, lap_pos, diff in zip(races, grid_positions, lap_positions, differences):
+        color = "green" if diff > 0 else "red"
+        plt.plot(
+            [grid_pos, lap_pos], [race, race], color=color
+        )  # Draw a line between the grid position and lap position
+        plt.plot(grid_pos, race, "D", color="C0")  # Plot the grid position
+        plt.plot(lap_pos, race, "o", color="C1")  # Plot the lap position
 
     # Adding labels and title
     plt.xlabel("Position")
     plt.ylabel("Race")
-    plt.title(f"Grid Position vs. Lap 1 Position for {specific_driver} - {year} Season")
+    plt.title(f"{specific_driver} {year} - Grid Position vs. Lap 1 End Position")
     plt.xticks(rotation=0)  # Rotate race names for better readability
-    plt.xlim(1, 20)  # Set x-axis limits from 1 to 20
+    plt.xlim(1, 21)  # Set x-axis limits from 1 to 20
     plt.xticks(range(1, 21))  # Ensure x-axis has ticks from 1 to 20
-    plt.legend(["Grid Position", "Lap 1 End Position"])
-    plt.grid(True)
+    # plt.legend(["Grid Position", "Lap 1 End Position"])
+
+    # Custom legend
+    plt.legend(custom_lines, ["Gained Positions", "Lost Positions", "Grid Position", "Lap 1 End Position"])
+
+    plt.grid(visible=True)
     plt.tight_layout()
 
     if save_to_file:
